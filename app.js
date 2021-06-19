33b42f007f643b2de8becb070a6671fde07549aa
ahmedfarah2 <a.farah@student.fontys.nl>
2021-06-19 13:34:27 +0200

    app.js

--- app.js
+++ app.js
@@ -1,30 +1,8 @@
-const http = require('http')
-const fs = require('fs')
-const port = 3000
+const express = require("express");
+const app = express();
 
-const server = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' })
-  fs.readFile('index.html', function (error, data) {
-    if (error) {
-      res.writeHead(404)
-      res.write('Error : File not found')
-    } else {
-      res.write(data)
-    }
-    res.end()
-  })
+app.get("/", function (req, res) {
+  res.send("Working!");
+});
 
-})
-
-  server.listen(port, function (error) {
-    if (error) {
-      console.log('Error found, something went wrong', error)
-    } else {
-      console.log('Server is listening on port ' + port)
-    }
-  })
-
-
-
-
-
+app.listen(process.env.PORT || 5000);
