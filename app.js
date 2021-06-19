7f65074816edfd7b0ff806f1af1a83fe2987d99c
ahmedfarah2 <a.farah@student.fontys.nl>
2021-06-19 13:42:24 +0200

    update app.js

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
 
-// app.get("/", function (req, res) {
-//   res.send("Working!");
-// });
-
-// app.listen(process.env.PORT || 5000);
-
-var http = require('http'),
-    fs = require('fs');
-
-
-fs.readFile('index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
+app.get("/", function (req, res) {
+  // res.send("Working!");
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
+
+app.listen(process.env.PORT || 5000);
+
+// var http = require('http'),
+//     fs = require('fs');
+
+
+// fs.readFile('index.html', function (err, html) {
+//     if (err) {
+//         throw err; 
+//     }       
+//     http.createServer(function(request, response) {  
+//         response.writeHeader(200, {"Content-Type": "text/html"});  
+//         response.write(html);  
+//         response.end();  
+//     }).listen(8000);
+// });
