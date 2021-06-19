58a9188e5a3216885979417c674605a20d3a28ba
ahmedfarah2 <a.farah@student.fontys.nl>
2021-06-19 13:37:25 +0200

    read html file

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Working!");
+// app.get("/", function (req, res) {
+//   res.send("Working!");
+// });
+
+// app.listen(process.env.PORT || 5000);
+
+var http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
 });
-
-app.listen(process.env.PORT || 5000);
