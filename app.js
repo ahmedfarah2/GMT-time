9e800d92279be7bd750cb1a680b1a852814bcd44
ahmedfarah2 <a.farah@student.fontys.nl>
2021-06-19 13:39:27 +0200

    app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var http = require('http'),
     fs = require('fs');
 
 
-fs.readFile('./index.html', function (err, html) {
+fs.readFile('index.html', function (err, html) {
     if (err) {
         throw err; 
     }       
